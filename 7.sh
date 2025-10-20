7aa7bc4911d90da2c31530bc8e1e6a4d9b1772e0
Blackhazelnutt <baallqiisss@gmail.com>
2025-10-20 10:40:05 +0700

    Update 7.sh

---
+++ 7.sh
@@ -0,0 +1,11 @@
+# Node Tirion
+
+# nano /etc/bind/k41.com
+
+```
+
+www     IN      CNAME   sirion.k41.com.
+static  IN      CNAME   lindon.k41.com.
+app     IN      CNAME   vingilot.k41.com.
+
+```

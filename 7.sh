e751a5286b54a2261e8362afbc26440f44a02c9b
Blackhazelnutt <baallqiisss@gmail.com>
2025-10-20 10:44:54 +0700

    Update 7.sh

--- 7.sh
+++ 7.sh
@@ -9,3 +9,9 @@ static  IN      CNAME   lindon.k41.com.
 app     IN      CNAME   vingilot.k41.com.
 
 ```
+# service bind9 restart
+
+# Tes 
+` dig www.k41.com CNAME +short `
+` dig app.k41.com CNAME +short '
+` dig static.k41.com CNAME +short '

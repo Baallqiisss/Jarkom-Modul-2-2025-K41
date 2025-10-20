d51eab5d3e86e9d796c046ecdd967710749eb11a
Blackhazelnutt <baallqiisss@gmail.com>
2025-10-20 11:06:08 +0700

    Remove commented-out DNS records from 7.sh

--- 7.sh
+++ 7.sh
@@ -1,15 +1,1 @@
-# Node Tirion
-
-# nano /etc/bind/k41.com
-
-www     IN      CNAME   sirion.k41.com.
-static  IN      CNAME   lindon.k41.com.
-app     IN      CNAME   vingilot.k41.com.
-
-# service bind9 restart
-
-# Tes 
-dig www.k41.com CNAME +short 
-dig app.k41.com CNAME +short 
-dig static.k41.com CNAME +short 
 
